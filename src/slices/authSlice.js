162b5636179fd29e86aa0a7640ab18c1f3b0ac31
DishankM <dishankmahajan424@gmail.com>
2025-12-19 09:21:04 +0530

    Api Updated

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,26 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) :  null,
+  token: localStorage.getItem("token") || null,
+  loading: false,
+  signupData: null,
 };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState: initialState,
-    reducers:{
-        setSignupData(state, value) {
-            state.signupData = value.payload;
-          },
-          setLoading(state, value) {
-            state.loading = value.payload;
-          },
-        setToken(state, value){
-            state.token = value.payload;
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setSignupData(state, action) {
+      state.signupData = action.payload;
+    },
+    setLoading(state, action) {
+      state.loading = action.payload;
+    },
+    setToken(state, action) {
+      state.token = action.payload;
+      // keep token in localStorage
+      localStorage.setItem("token", action.payload);
+    },
+    logout(state) {
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+});
 
-export const {setToken, setLoading, setSignupData} = authSlice.actions;
+export const { setToken, setLoading, setSignupData, logout } =
+  authSlice.actions;
+
 export default authSlice.reducer;
-
-
